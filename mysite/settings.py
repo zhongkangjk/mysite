f0cd863a01b28850e50e8fd7d981dee9d31ff19c
zhongkangjk <296053096@qq.com>
2020-02-22 17:10:05 +0800

    做完了例子除了测试

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'simpleui',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -67,6 +68,13 @@ TEMPLATES = [
         },
     },
 ]
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
--- polls/admin.py
+++ polls/admin.py
@@ -5,7 +5,19 @@ from django.contrib import admin
 
 from .models import Question,Choice
 
+
+#class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
+    #fields = ['pub_date', 'question_text']#顺序
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('时间选择大项', {'fields': ['pub_date']}),
+    ]
+
     list_display = ('id','question_text','pub_date')
 
     list_per_page = 50
@@ -24,17 +36,23 @@ class QuestionAdmin(admin.ModelAdmin):
     #详细时间分层筛选
     date_hierarchy = 'pub_date'
 
+    inlines = [ChoiceInline]
 
 
 
-class ChoiceAdmin(admin.ModelAdmin):
-    list_display = ('id','question','choice_text','votes')
 
-    list_per_page = 50
 
-    list_editable = ['question','choice_text']
+# class ChoiceAdmin(admin.ModelAdmin):
+#     list_display = ('id','question','choice_text','votes')
+
+#     list_per_page = 50
+
+#     list_editable = ['question','choice_text']
+
+
+
 
 admin.site.register(Question,QuestionAdmin)
-admin.site.register(Choice,ChoiceAdmin)
+# admin.site.register(Choice,ChoiceAdmin)
 admin.site.site_header = '后台'
 admin.site.site_title = '后台的标题'
